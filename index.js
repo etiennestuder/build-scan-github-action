f3421b76d02965375a84ddc9d7bfd5e1abf2d7f7
Etienne Studer <etienne@studer.nu>
2020-10-30 19:05:59 +0100

    Use single-quotes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
 
 try {
     // `who-to-greet` input defined in action metadata file
@@ -7,7 +8,7 @@ try {
     console.log(`Hello ${nameToGreet}!`);
 
     const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
+    core.setOutput('time', time);
 
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
